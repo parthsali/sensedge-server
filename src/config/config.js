e9c222df153111dc10ce149a08a13f4b68084132
ParthSali <parthsali04@gmail.com>
2025-02-11 19:20:13 +0530

    add env variables

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,11 @@ configEnv();
 const _config = {
   PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
+  NODE_ENV: process.env.NODE_ENV,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRATION: process.env.JWT_EXPIRATION,
+  EMAIL_USER: process.env.EMAIL_USER,
+  EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
 };
 
 export const config = Object.freeze(_config);
