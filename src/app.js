76569268f5804b8217c5d425212ff753e8de88e6
ParthSali <parthsali04@gmail.com>
2025-02-11 19:22:08 +0530

    add routes in app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import express from "express";
+
+import authRouter from "./api/auth/authRouter.js";
+import userRouter from "./api/user/userRouter.js";
+import adminRouter from "./api/admin/adminRouter.js";
+
 import globalErrorHandler from "./middlewares/globalErrorHandler.js";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next) => {
   res.status(200).json({ message: "Welcome to Sensedge APIs" });
 });
+
+app.get("/health", (req, res, next) => {
+  res.status(200).json({ message: "API is running" });
+});
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/admin", adminRouter);
 
 app.use(globalErrorHandler);
 
