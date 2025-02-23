23fcec1363295aed92750557f95d103957bb4d78
ParthSali <parthsali04@gmail.com>
2025-02-23 19:54:06 +0530

    Add CORS middleware

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 import express from "express";
+import cors from "cors";
 
 import authRouter from "./api/auth/authRouter.js";
 import userRouter from "./api/user/userRouter.js";
 import adminRouter from "./api/admin/adminRouter.js";
 import customerRouter from "./api/customer/customerRouter.js";
 import templateRouter from "./api/template/templateRouter.js";
+// import conversationRouter from "./api/conversation/conversationRouter.js";
+// import messageRouter from "./api/message/messageRouter.js";
 
 import globalErrorHandler from "./middlewares/globalErrorHandler.js";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/", (req, res, next) => {
   res.status(200).json({ message: "Welcome to Sensedge APIs" });
@@ -25,6 +29,8 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/customers", customerRouter);
 app.use("/api/v1/templates", templateRouter);
+// app.use("/api/v1/conversations", conversationRouter);
+// app.use("/api/v1/messages", messageRouter);
 
 app.use(globalErrorHandler);
 
