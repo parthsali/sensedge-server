00b47d1c473832a66697610946ff5225f58c3705
ParthSali <parthsali04@gmail.com>
2025-02-25 00:07:56 +0530

    add template validations

--- src/api/template/templateValidation.js
+++ src/api/template/templateValidation.js
@@ -3,10 +3,4 @@ import Joi from "joi";
 export const createTemplateSchema = Joi.object({
   name: Joi.string().required(),
   content: Joi.string().required(),
-  //   mediaFiles: Joi.array().items(
-  //     Joi.object({
-  //       url: Joi.string().required(),
-  //       type: Joi.string().valid("image", "pdf", "video", "other").required(),
-  //     })
-  //   ),
 });
