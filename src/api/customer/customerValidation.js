1a26f714fec8eca931b9e117683995355c5b835a
OzDiya <oswaldiya6@gmail.com>
2025-03-24 14:45:47 +0530

    update create customer validation

--- src/api/customer/customerValidation.js
+++ src/api/customer/customerValidation.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const customerValidation = Joi.object({
   name: Joi.string().required(),
-  phone: Joi.string().required().min(10).max(10),
+  phone: Joi.string().required().min(10).max(13),
   company: Joi.string().required(),
   assigned_user: Joi.string().required(),
 });
