20af082e1d123f27b2b1a665b52f816c17d64682
OzDiya <oswaldiya6@gmail.com>
2025-03-26 14:51:51 +0530

    fix receive image in webhook

--- src/api/message/messageController.js
+++ src/api/message/messageController.js
@@ -474,6 +474,7 @@ export const handleWebhook = async (req, res, next) => {
                   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
               },
             });
+            console.log("Response after fetch", response);
             if (!response.ok) {
               console.error(
                 "Error fetching file:",
