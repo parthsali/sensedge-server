97f51b5df4f38c15f0fe18736230ea3153bc4748
Parth <parthsali04@gmail.com>
2025-04-06 16:00:08 +0530

    fix search user

--- src/api/user/userController.js
+++ src/api/user/userController.js
@@ -1,6 +1,7 @@
 import Config from "./configModel.js";
 import createHttpError from "http-errors";
 import User from "./userModel.js";
+import Customer from "../customer/customerModel.js";
 
 export const getDefaultUser = async (req, res, next) => {
   try {
@@ -66,14 +67,37 @@ export const searchUser = async (req, res, next) => {
       throw createHttpError(400, "Query is required");
     }
 
-    const users = await User.find({
-      $or: [
-        { name: { $regex: query, $options: "i" } },
-        { email: { $regex: query, $options: "i" } },
-      ],
-    });
+    const users = await User.find(
+      {
+        role: "user",
+        $or: [
+          { name: { $regex: query, $options: "i" } },
+          { email: { $regex: query, $options: "i" } },
+        ],
+      },
+      {
+        password: 0,
+      }
+    );
 
-    res.status(200).json({ message: "Users fetched", users });
+    const usersData = [];
+    for (const user of users) {
+      const customers = await Customer.find({ assigned_user: user._id });
+
+      usersData.push({
+        ...user._doc,
+        customers,
+      });
+    }
+
+    // get total count of users
+    const totalUsers = await User.countDocuments({ role: "user" });
+
+    res.status(200).json({
+      message: "Users fetched",
+      users: usersData,
+      totalUsers,
+    });
   } catch (error) {
     next(error);
   }
@@ -107,8 +131,7 @@ export const setAdmin = async (req, res, next) => {
     await config.save();
 
     res.status(200).json({ message: "Admin set", admin });
- 
   } catch (error) {
     next(error);
   }
-}
+};
