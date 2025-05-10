8f20b03dd1e52617fcb65e556e1b6a9a93176953
Parth <parthsali04@gmail.com>
2025-05-10 12:38:57 +0530

    add logs

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -16,11 +16,11 @@ const logger = winston.createLogger({
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: "info.log", level: "info" }),
-    new winston.transports.File({ filename: "warn.log", level: "warn" }),
-    new winston.transports.File({ filename: "debug.log", level: "debug" }),
-    new winston.transports.File({ filename: "error.log", level: "error" }),
-    new winston.transports.File({ filename: "combined.log" }),
+    new winston.transports.File({ filename: "logs/info.log", level: "info" }),
+    new winston.transports.File({ filename: "logs/warn.log", level: "warn" }),
+    new winston.transports.File({ filename: "logs/debug.log", level: "debug" }),
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+    new winston.transports.File({ filename: "logs/combined.log" }),
   ],
 });
 
